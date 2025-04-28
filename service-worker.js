869150dc7078deca219b8f9df8bf4b6508d4d91f
SweetlyGlobal <sweetlyglobal7@gmail.com>
2025-04-28 17:24:55 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+// This is the simplest service worker to make your PWA installable
+
+self.addEventListener('install', function(event) {
+    console.log('[Service Worker] Install Event');
+    self.skipWaiting(); // Forces waiting service worker to become active
+  });
+  
+  self.addEventListener('activate', function(event) {
+    console.log('[Service Worker] Activate Event');
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    // Just fetch everything normally
+    event.respondWith(fetch(event.request));
+  });
